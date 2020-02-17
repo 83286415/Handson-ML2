43106549a9b4ee10ba1e83f3a16e11af942ce325
83286415 <83286415@163.com>
2020-02-17 18:50:02 +0800

    chapter10_introduction_to_artificial_neural_networks_with_keras.py: perceptrons

--- scripts/chapter10_introduction_to_artificial_neural_networks_with_keras.py
+++ scripts/chapter10_introduction_to_artificial_neural_networks_with_keras.py
@@ -13,6 +13,11 @@ assert tf.__version__ >= "2.0"
 # Common imports
 import numpy as np
 import os
+from matplotlib.colors import ListedColormap
+
+# chapter imports
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
 
 # to make this notebook's output stable across runs
 np.random.seed(42)
@@ -42,3 +47,49 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
 # Ignore useless warnings (see SciPy issue #5998)
 import warnings
 warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+if __name__ == '__main__':
+
+    # Perceptrons
+
+    # data set
+    iris = load_iris()
+    X = iris.data[:, (2, 3)]  # petal length, petal width
+    y = (iris.target == 0).astype(np.int)  # choose the first kind of iris
+
+    # build model
+    per_clf = Perceptron(max_iter=1000, tol=1e-3, random_state=42)
+    per_clf.fit(X, y)
+
+    y_pred = per_clf.predict([[2, 0.5]])
+    print(y_pred)  # [1]
+
+    # plot
+    a = -per_clf.coef_[0][0] / per_clf.coef_[0][1]  # y=ax+b
+    b = -per_clf.intercept_ / per_clf.coef_[0][1]
+
+    axes = [0, 5, 0, 2]
+
+    x0, x1 = np.meshgrid(
+        np.linspace(axes[0], axes[1], 500).reshape(-1, 1),
+        np.linspace(axes[2], axes[3], 200).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+    y_predict = per_clf.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(X[y == 0, 0], X[y == 0, 1], "bs", label="Not Iris-Setosa")
+    plt.plot(X[y == 1, 0], X[y == 1, 1], "yo", label="Iris-Setosa")
+
+    plt.plot([axes[0], axes[1]], [a * axes[0] + b, a * axes[1] + b], "k-", linewidth=3)
+    custom_cmap = ListedColormap(['#9898ff', '#fafab0'])
+
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(loc="lower right", fontsize=14)
+    plt.axis(axes)
+
+    save_fig("perceptron_iris_plot")
+    plt.show()
