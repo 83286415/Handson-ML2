2b896ea66034b4a6578022cd803e818ab4e9e022
83286415 <83286415@163.com>
2020-03-10 18:40:53 +0800

    chapter11_faster_optimizers.py - Adam, AdaMax, Nadam and Learning Rate Power Scheduling

--- scripts/chapter11_faster_optimizers.py
+++ scripts/chapter11_faster_optimizers.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
 
     optimizer_momentum = keras.optimizers.SGD(lr=0.001, momentum=0.9)  # It's easy to add momentum optimizer. Just add this para.
 
-    # Nesterov Accelerated Gradient
+    # Nesterov Accelerated Gradient - NAG
 
     optimizer_nesterov = keras.optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True)
 
@@ -84,4 +84,72 @@ if __name__ == '__main__':
     # RMSProp
 
     optimizer_rmsprop = keras.optimizers.RMSprop(lr=0.001, rho=0.9)  # rho: the β mentioned in book
-    # prefer this RMSProp than AdaGrad above.
+    # prefer this RMSProp than AdaGrad above. Refer to cloud note.
+
+    # Adam Optimization
+
+    optimizer_adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+
+    # Adamax Optimization
+
+    optimizer_adamax = keras.optimizers.Adamax(lr=0.001, beta_1=0.9, beta_2=0.999)
+
+    # Nadam Optimization
+
+    optimizer_nadam = keras.optimizers.Nadam(lr=0.001, beta_1=0.9, beta_2=0.999)
+
+    # Learning Rate Scheduling
+
+    # Power Scheduling
+
+    # formula: lr = lr0 / (1 + steps / s)**c  Keras uses c=1 and s = 1 / decay
+    # so lr = lr0 / (1 + steps*decay)
+
+    # data set
+    (X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
+    X_train_full = X_train_full / 255.0
+    X_test = X_test / 255.0
+    X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
+    y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+    pixel_means = X_train.mean(axis=0, keepdims=True)
+    pixel_stds = X_train.std(axis=0, keepdims=True)
+    X_train_scaled = (X_train - pixel_means) / pixel_stds
+    X_valid_scaled = (X_valid - pixel_means) / pixel_stds
+    X_test_scaled = (X_test - pixel_means) / pixel_stds
+
+    # optimizer
+    optimizer = keras.optimizers.SGD(lr=0.01, decay=1e-4)
+
+    # build model
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    n_epochs = 25
+    history = model.fit(X_train_scaled, y_train, epochs=n_epochs,
+                        validation_data=(X_valid_scaled, y_valid))
+
+    # plot
+    learning_rate = 0.01
+    decay = 1e-4
+    batch_size = 32
+    n_steps_per_epoch = len(X_train) // batch_size
+    epochs = np.arange(n_epochs)
+    lrs = learning_rate / (1 + decay * epochs * n_steps_per_epoch)  # the formula above: lr = lr0 / (1 + steps*decay)
+
+    plt.plot(epochs, lrs, "o-")
+    plt.axis([0, n_epochs - 1, 0, 0.01])
+    plt.xlabel("Epoch")
+    plt.ylabel("Learning Rate")
+    plt.title("Power Scheduling", fontsize=14)
+    plt.grid(True)
+
+    save_fig("power_scheduling")
+    # plt.show()
+
+    # Exponential Scheduling
