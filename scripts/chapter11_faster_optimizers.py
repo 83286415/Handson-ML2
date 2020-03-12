2bbf53bc90a401374efb4d9ace739b62d1bf546d
83286415 <83286415@163.com>
2020-03-12 17:40:24 +0800

    chapter11_faster_optimizers.py - Piecewise, Performance and 1cycle Scheduling

--- scripts/chapter11_faster_optimizers.py
+++ scripts/chapter11_faster_optimizers.py
@@ -101,6 +101,88 @@ class ExponentialDecay(keras.callbacks.Callback):
         logs['lr'] = K.get_value(self.model.optimizer.lr)
 
 
+def piecewise_constant_fn(epoch):
+    if epoch < 5:
+        return 0.01
+    elif epoch < 15:
+        return 0.005
+    else:
+        return 0.001
+
+
+def piecewise_constant(boundaries, values):
+    boundaries = np.array([0] + boundaries)
+    values = np.array(values)
+
+    def piecewise_constant_fn(epoch):
+        return values[np.argmax(boundaries > epoch) - 1]
+    return piecewise_constant_fn
+
+
+class ExponentialLearningRate(keras.callbacks.Callback):
+    def __init__(self, factor):
+        self.factor = factor
+        self.rates = []
+        self.losses = []
+
+    def on_batch_end(self, batch, logs):
+        self.rates.append(K.get_value(self.model.optimizer.lr))
+        self.losses.append(logs["loss"])
+        K.set_value(self.model.optimizer.lr, self.model.optimizer.lr * self.factor)
+
+
+def find_learning_rate(model, X, y, epochs=1, batch_size=32, min_rate=10**-5, max_rate=10):
+    init_weights = model.get_weights()
+    iterations = len(X) // batch_size * epochs
+    factor = np.exp(np.log(max_rate / min_rate) / iterations)
+    init_lr = K.get_value(model.optimizer.lr)
+    K.set_value(model.optimizer.lr, min_rate)
+    exp_lr = ExponentialLearningRate(factor)
+    history = model.fit(X, y, epochs=epochs, batch_size=batch_size,
+                        callbacks=[exp_lr])
+    K.set_value(model.optimizer.lr, init_lr)
+    model.set_weights(init_weights)
+    return exp_lr.rates, exp_lr.losses
+
+
+def plot_lr_vs_loss(rates, losses):
+    plt.plot(rates, losses)
+    plt.gca().set_xscale('log')
+    plt.hlines(min(losses), min(rates), max(rates))
+    plt.axis([min(rates), max(rates), min(losses), (losses[0] + min(losses)) / 2])
+    plt.xlabel("Learning rate")
+    plt.ylabel("Loss")
+
+
+class OneCycleScheduler(keras.callbacks.Callback):
+    def __init__(self, iterations, max_rate, start_rate=None,
+                 last_iterations=None, last_rate=None):
+        self.iterations = iterations
+        self.max_rate = max_rate
+        self.start_rate = start_rate or max_rate / 10
+        self.last_iterations = last_iterations or iterations // 10 + 1
+        self.half_iteration = (iterations - self.last_iterations) // 2
+        self.last_rate = last_rate or self.start_rate / 1000
+        self.iteration = 0
+
+    def _interpolate(self, iter1, iter2, rate1, rate2):
+        return ((rate2 - rate1) * (self.iteration - iter1)
+                / (iter2 - iter1) + rate1)
+
+    def on_batch_begin(self, batch, logs):
+        if self.iteration < self.half_iteration:
+            rate = self._interpolate(0, self.half_iteration, self.start_rate, self.max_rate)
+        elif self.iteration < 2 * self.half_iteration:
+            rate = self._interpolate(self.half_iteration, 2 * self.half_iteration,
+                                     self.max_rate, self.start_rate)
+        else:
+            rate = self._interpolate(2 * self.half_iteration, self.iterations,
+                                     self.start_rate, self.last_rate)
+            rate = max(rate, self.last_rate)
+        self.iteration += 1
+        K.set_value(self.model.optimizer.lr, rate)
+
+
 if __name__ == '__main__':
 
     # Momentum Optimization
@@ -288,3 +370,117 @@ if __name__ == '__main__':
     plt.show()
 
     # Piecewise Constant Scheduling
+
+    # callback: define piece-wise constant scheduling callback
+    piecewise_constant_fn = piecewise_constant([5, 15], [0.01, 0.005, 0.001])
+    lr_scheduler = keras.callbacks.LearningRateScheduler(piecewise_constant_fn)
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="nadam", metrics=["accuracy"])
+
+    n_epochs = 25
+    piece_wise_constant_history = model.fit(X_train_scaled, y_train, epochs=n_epochs,
+                                            validation_data=(X_valid_scaled, y_valid),
+                                            callbacks=[lr_scheduler])
+
+    # plot
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(history.epoch, [piecewise_constant_fn(epoch) for epoch in history.epoch], "o-")
+    plt.axis([0, n_epochs - 1, 0, 0.011])
+    plt.xlabel("Epoch")
+    plt.ylabel("Learning Rate")
+    plt.title("Piecewise Constant Scheduling", fontsize=14)
+    plt.grid(True)
+    # plt.show()
+
+    # Performance Scheduling
+
+    tf.random.set_random_seed(42)
+    np.random.seed(42)
+
+    lr_scheduler = keras.callbacks.ReduceLROnPlateau(factor=0.5, patience=5)  # reduce learning rate on plateau
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    optimizer = keras.optimizers.SGD(lr=0.02, momentum=0.9)  # momentum
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    n_epochs = 25
+    performance_schedule_history = model.fit(X_train_scaled, y_train, epochs=n_epochs,
+                                             validation_data=(X_valid_scaled, y_valid),
+                                             callbacks=[lr_scheduler])
+
+    # plot
+    plt.figure(figsize=(6.4, 4.8))
+    lr_scheduler = keras.callbacks.ReduceLROnPlateau(factor=0.5, patience=5)
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    optimizer = keras.optimizers.SGD(lr=0.02, momentum=0.9)
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    n_epochs = 25
+    history = model.fit(X_train_scaled, y_train, epochs=n_epochs,
+                        validation_data=(X_valid_scaled, y_valid),
+                        callbacks=[lr_scheduler])
+
+    # tf.keras API
+
+    # Exponential Scheduling
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    s = 20 * len(X_train) // 32  # number of steps in 20 epochs (batch size = 32), a big number for all epochs.
+    learning_rate = keras.optimizers.schedules.ExponentialDecay(0.01, s, 0.1)
+    optimizer = keras.optimizers.SGD(learning_rate)
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    n_epochs = 25
+    tf_keras_api_history = model.fit(X_train_scaled, y_train, epochs=n_epochs,
+                                     validation_data=(X_valid_scaled, y_valid))
+
+    # Piecewise Constant Scheduling
+    learning_rate = keras.optimizers.schedules.PiecewiseConstantDecay(
+        boundaries=[5. * n_steps_per_epoch, 15. * n_steps_per_epoch],
+        values=[0.01, 0.005, 0.001])
+
+    # 1Cycle scheduling
+
+    tf.random.set_random_seed(42)
+    np.random.seed(42)
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(100, activation="selu", kernel_initializer="lecun_normal"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    model.compile(loss="sparse_categorical_crossentropy",
+                  optimizer=keras.optimizers.SGD(lr=1e-3),
+                  metrics=["accuracy"])
+
+    batch_size = 128
+    rates, losses = find_learning_rate(model, X_train_scaled, y_train, epochs=1, batch_size=batch_size)
+    plot_lr_vs_loss(rates, losses)
+
+    n_epochs = 25
+    onecycle = OneCycleScheduler(len(X_train) // batch_size * n_epochs, max_rate=0.05)
+    onecycle_history = model.fit(X_train_scaled, y_train, epochs=n_epochs, batch_size=batch_size,
+                                 validation_data=(X_valid_scaled, y_valid),
+                                 callbacks=[onecycle])
